0b717f1b304d2a66f2c9b422c8db9f3b49dc4a53
Francisco LR <francisco.lopez247037@potros.itson.edu.mx>
2024-11-10 02:43:58 -0700

    fix
    fix de un error con la conexión de la bd

--- pf_appweb_Inges/src/main/java/daos/ConexionBD.java
+++ pf_appweb_Inges/src/main/java/daos/ConexionBD.java
@@ -12,11 +12,16 @@ import java.sql.SQLException;
  * @author Fran
  */
 public class ConexionBD {
-    private static final String URL="jdbc:mysql://localhost:3306/red_social";
+    private static final String URL="jdbc:mysql://localhost:3306/red_social?zeroDateTimeBehavior=CONVERT_TO_NULL";
     private static final String USER = "root";
     private static final String PASSWORD ="fran";
 
-    public static Connection getConnection() throws SQLException{
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
