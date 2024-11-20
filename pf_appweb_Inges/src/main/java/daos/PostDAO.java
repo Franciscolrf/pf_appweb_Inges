b64175283cc9bf835fcea27dd5ab5c045f7a3b05
Francisco LR <francisco.lopez247037@potros.itson.edu.mx>
2024-11-19 22:39:06 -0700

    metodo para agregar post

--- pf_appweb_Inges/src/main/java/daos/PostDAO.java
+++ pf_appweb_Inges/src/main/java/daos/PostDAO.java
@@ -4,7 +4,11 @@
  */
 package daos;
 
+import dtos.PostDTO;
 import interfaces.IPostDAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import modelo.Post;
 
 /**
@@ -13,28 +17,40 @@ import modelo.Post;
  */
 public class PostDAO implements IPostDAO {
 
-    @Override
-    public void agregarPost(Post post) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'agregarPost'");
-    }
-
-    @Override
-    public void modificarPost(Post post) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'modificarPost'");
-    }
-
-    @Override
-    public void eliminarPost(Post post) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'eliminarPost'");
-    }
 
     @Override
     public Post consultarPost(int id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'consultarPost'");
+    }
+
+    @Override
+    public boolean agregarPost(PostDTO post) {
+         String sql = "INSERT INTO posts (titulo, contenido, anclado, usuario_id) VALUES (?, ?, ?, ?)";
+        try (Connection connection = ConexionBD.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, post.getTitulo());
+            statement.setString(2, post.getContenido());
+            statement.setBoolean(3, post.isAnclado());
+            statement.setLong(4, post.getUsuario().getId());
+
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    
+    }
+
+    @Override
+    public void modificarPost(PostDTO post) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminarPost(PostDTO post) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
